8cab66625c6ea0afb26a9077b41b49768c8ef77d
Nicola Brisotto <nicolabrisotto@gmail.com>
2014-04-23 00:12:39 +0200

    increase timeout to avoid errors on slow connection

--- recipes/buildstack.rb
+++ recipes/buildstack.rb
@@ -2,11 +2,13 @@
 if node['dokku']['buildstack']['use_prebuilt']
   docker_image node['dokku']['buildstack']['image_name'] do
     source node['dokku']['buildstack']['prebuilt_url']
+    cmd_timeout 600
     action :import
   end
 else
   docker_image node['dokku']['buildstack']['image_name'] do
     source node['dokku']['buildstack']['stack_url']
+    cmd_timeout 600
     action :build
   end
 end
